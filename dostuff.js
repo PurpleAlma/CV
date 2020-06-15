0803a69351caa52aba7208e803ac60f9e8ff1796
PurpleAlma <64203600+PurpleAlma@users.noreply.github.com>
2020-06-15 20:27:34 +0300

    Update dostuff.js

--- dostuff.js
+++ dostuff.js
@@ -1,13 +1,17 @@
-const grid_1=document.querySelector('.item-1')
-$(document).ready(function(){
-    $('#toggleh').on('click', function () {
-        $(this).text((i, t) => t == 'Welcome to my ' ? 'Home Page' : 'Welcome to my ');
-    });
-});
-$(document).ready(function(){
-    $('#test2').on('click', function () {
-        $(this).text((i, t) => t == 'Welcome to my ' ? 'Home Page' : 'Welcome to my ');
-    });
+$('.header').hover(
+    function(){$(this).css("opacity",1)},
+    function(){$(this).css("opacity",0.8)})
+
+$('.footer').hover(
+    function(){$(this).css("opacity",1)},
+    function(){$(this).css("opacity",0.8)})
+
+$(".boring-summary-button").click(function(){
+    $(".boring-summary").toggle(500)
 });
 
-//https://purplealma.github.io/CV/home
+$(document).ready(function(){
+    $('.boring-summary-button').on('click', function () {
+        $(this).html((i, t) => t == 'Hide' ? 'Show' : 'Hide');
+    });
+});
